963c9ad8fbdcfc748c2349d1244d6617e1f7fe12
armanabbassii <arman.abbasi.nt@gmail.com>
2023-08-26 21:55:28 +0330

    delete unnecessary commands

--- cypress/e2e/1-getting-started/LoginWithCypress.cy.js
+++ cypress/e2e/1-getting-started/LoginWithCypress.cy.js
@@ -21,30 +21,16 @@ Cypress.Commands.add("getLoggedInSession", () => {
       //   cy.log("name: ", cookie.name, " value: ", cookie.value);
       cy.setCookie(cookie.name, cookie.value, { log: false });
     });
-
-    // cy.contains("مدیریت کاربران").click();
   });
   // cy.reload();
 });
-// });
-// });
 
-// Cypress.Commands.add("getLoggedInSession", () => {
-//   cy.log("Hello there");
-// });
-// start
-
-// describe("Playwright Login with Cypress Integration", () => {
 it("The test can be simplified with a Cypress.command", () => {
   // cy.visit("http://dev.cms.test/");
   // cy.get(".login-btn").contains("ورود با نام کاربری").click();
-  // cy.log(
-  //   "Creating Playwright session and pulling cookies to apply to Cypress."
-  // );
 
+  //   "Creating Playwright session and pulling cookies to apply to Cypress."
   cy.getLoggedInSession();
-  // cy.contains('YOUR_BUTTON_CLASS', 'Customer');
 
   // cy.reload();
 });
-// });
