71bc0cc3d4b92818895d6aa14961d739e0856fb4
armanabbassii <arman.abbasi.nt@gmail.com>
2023-08-29 01:15:59 +0330

    check the new solution

--- cypress/e2e/1-getting-started/LoginWithCypress.cy.js
+++ cypress/e2e/1-getting-started/LoginWithCypress.cy.js
@@ -17,7 +17,7 @@
 
 Cypress.Commands.add("getLoggedInSession", () => {
   return cy.task("getLoggedInSession").then((cookies) => {
-    cy.log("Setting cookies from Playwright session", cookies);
+    cy.log("Setting cookies from Playwright session", JSON.stringify(cookies));
 
     cookies.forEach((cookie) => {
       //   cy.log("name: ", cookie.name, " value: ", cookie.value);
@@ -27,8 +27,8 @@ Cypress.Commands.add("getLoggedInSession", () => {
   // cy.reload();
 });
 it("The test can be simplified with a Cypress.command", () => {
-  // cy.visit("http://dev.cms.test/");
-  // cy.get(".login-btn").contains("ورود با نام کاربری").click();
+  cy.visit("http://dev.cms.test/");
+  cy.get(".login-btn").contains("ورود با نام کاربری").click();
 
   //   "Creating Playwright session and pulling cookies to apply to Cypress."
   cy.getLoggedInSession();
